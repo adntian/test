ed6df8833e4ff0b55105f6a54fca5db5f59b703b
adntian <china2008tian@126.com>
2023-04-25 09:13:45 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -17,6 +17,8 @@ DASHPASS=$1
 DASHPORT=${3:-443}
 SHMEXT=${4:-9001}
 SHMINT=${5:-10001}
+INT_IP=$(ip addr show | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1|grep 10)
+EXT_IP=$(curl -s http://checkip.dyndns.org | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
 if [ -z "$2" ]; then
   echo '不会设置ssh公钥'
 else
@@ -182,7 +184,8 @@ LOCALLANIP=$(get_ip)
 cd ${NODEHOME} &&
 touch ./.env
 cat >./.env <<EOL
-APP_IP=auto
+EXT_IP=${EXTERNALIP}
+INT_IP=${INTERNALIP}
 EXISTING_ARCHIVERS=[{"ip":"18.194.3.6","port":4000,"publicKey":"758b1c119412298802cd28dbfa394cdfeecc4074492d60844cc192d632d84de3"},{"ip":"139.144.19.178","port":4000,"publicKey":"840e7b59a95d3c5f5044f4bc62ab9fa94bc107d391001141410983502e3cde63"},{"ip":"139.144.43.47","port":4000,"publicKey":"7af699dd711074eb96a8d1103e32b589e511613ebb0c6a789a9e8791b2b05f34"},{"ip":"72.14.178.106","port":4000,"publicKey":"2db7c949632d26b87d7e7a5a4ad41c306f63ee972655121a37c5e4f52b00a542"}]
 APP_MONITOR=${APPMONITOR}
 DASHPASS=${DASHPASS}
