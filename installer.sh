42ab4156e14b810f6f9c874a5ec93066a84a3135
adntian <china2008tian@126.com>
2023-04-29 20:21:48 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -34,8 +34,8 @@ echo '安装docker'
 sudo apt install docker.io -y
 echo '安装docker-compose'
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-//sudo chmod +x /usr/local/bin/docker-compose
-//sudo chmod 666 /var/run/docker.sock
+#sudo chmod +x /usr/local/bin/docker-compose
+#sudo chmod 666 /var/run/docker.sock
 
 
 
