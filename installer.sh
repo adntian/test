e2d51069c7e83635121a26853bf02bdccc31e537
adntian <china2008tian@126.com>
2023-05-08 19:36:13 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -25,18 +25,59 @@ else
   echo $2 >> ~/.ssh/authorized_keys
 fi
 
-echo '开始初始化...'
-echo '更新apt'
-sudo apt update
-echo '安装apt-utils'
-# sudo apt install -y apt-utils
-echo '安装docker'
-sudo apt install docker.io -y
-echo '安装docker-compose'
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+isUpdate=$(whereis docker | grep bin)
+if [ -z "isUpdate" ]; then
+  echo 'docker已经安装'
+else
+  echo 'docker未安装，开始安装docker'
+  echo '开始初始化...'
+  echo '更新apt'
+  sudo apt update
+  echo '安装apt-utils'
+  # sudo apt install -y apt-utils
+  echo '安装docker'
+  sudo apt install docker.io -y
+  echo '安装docker-compose'
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
 #sudo chmod 666 /var/run/docker.sock
 
+REPLY=${REPLY:-y}
+WARNING_AGREE=${WARNING_AGREE:-y}
+
+# Check if any hashing command is available
+if ! (command -v openssl > /dev/null || command -v shasum > /dev/null || command -v sha256sum > /dev/null); then
+  echo "No supported hashing commands found."
+  # read -p "Would you like to install openssl? (y/n) " -n 1 -r
+  # echo
+
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    # Detect package manager and install openssl
+    if command -v apt-get > /dev/null; then
+      sudo apt-get update && sudo apt-get install -y openssl
+    elif command -v yum > /dev/null; then
+      sudo yum install -y openssl
+    elif command -v dnf > /dev/null; then
+      sudo dnf install -y openssl
+    else
+      echo "Your package manager is not supported. Please install openssl manually."
+      exit 1
+    fi
+  else
+    echo "Please install openssl, shasum, or sha256sum and try again."
+    exit 1
+  fi
+fi
+
+
+WARNING_AGREE=${WARNING_AGREE:-y}
+
+if [ $WARNING_AGREE != "y" ];
+then
+  echo "Diagnostic data collection agreement not accepted. Exiting installer."
+  exit
+fi
 
 
 # Check all things that will be needed for this script to succeed like access to docker and docker-compose
@@ -61,7 +102,7 @@ docker-safe() {
   fi
 
   if ! docker $@; then
-    echo "Trying again with sudo..."
+    echo "Trying again with sudo..." >&2
     sudo docker $@
   fi
 }
@@ -124,11 +165,92 @@ get_external_ip() {
   echo $external_ip
 }
 
+hash_password() {
+  local input="$1"
+  local hashed_password
+
+  # Try using openssl
+  if command -v openssl > /dev/null; then
+    hashed_password=$(echo -n "$input" | openssl dgst -sha256 -r | awk '{print $1}')
+    echo "$hashed_password"
+    return 0
+  fi
+
+  # Try using shasum
+  if command -v shasum > /dev/null; then
+    hashed_password=$(echo -n "$input" | shasum -a 256 | awk '{print $1}')
+    echo "$hashed_password"
+    return 0
+  fi
+
+  # Try using sha256sum
+  if command -v sha256sum > /dev/null; then
+    hashed_password=$(echo -n "$input" | sha256sum | awk '{print $1}')
+    echo "$hashed_password"
+    return 0
+  fi
+
+  return 1
+}
+
 if [[ $(docker-safe info 2>&1) == *"Cannot connect to the Docker daemon"* ]]; then
     echo "Docker daemon is not running"
     exit 1
 else
     echo "Docker daemon is running"
+fi
+
+CURRENT_DIRECTORY=$(pwd)
+
+# DEFAULT VALUES FOR USER INPUTS
+DASHPORT_DEFAULT=8080
+EXTERNALIP_DEFAULT=auto
+INTERNALIP_DEFAULT=auto
+SHMEXT_DEFAULT=9001
+SHMINT_DEFAULT=10001
+PREVIOUS_PASSWORD=none
+
+#Check if container exists
+IMAGE_NAME="registry.gitlab.com/shardeum/server:latest"
+CONTAINER_ID=$(docker-safe ps -qf "ancestor=local-dashboard")
+if [ ! -z "${CONTAINER_ID}" ]; then
+  echo "CONTAINER_ID: ${CONTAINER_ID}"
+  echo "Existing container found. Reading settings from container."
+
+  # Assign output of read_container_settings to variable
+  if ! ENV_VARS=$(docker inspect --format="{{range .Config.Env}}{{println .}}{{end}}" "$CONTAINER_ID"); then
+    ENV_VARS=$(sudo docker inspect --format="{{range .Config.Env}}{{println .}}{{end}}" "$CONTAINER_ID")
+  fi
+
+  if ! docker-safe cp "${CONTAINER_ID}:/home/node/app/cli/build/secrets.json" ./; then
+    echo "Container does not have secrets.json"
+  else
+    echo "Reusing secrets.json from container"
+  fi
+
+  docker-safe stop "${CONTAINER_ID}"
+  docker-safe rm "${CONTAINER_ID}"
+
+  # UPDATE DEFAULT VALUES WITH SAVED VALUES
+  DASHPORT_DEFAULT=$(echo $ENV_VARS | grep -oP 'DASHPORT=\K[^ ]+') || DASHPORT_DEFAULT=8080
+  EXTERNALIP_DEFAULT=$(echo $ENV_VARS | grep -oP 'EXT_IP=\K[^ ]+') || EXTERNALIP_DEFAULT=auto
+  INTERNALIP_DEFAULT=$(echo $ENV_VARS | grep -oP 'INT_IP=\K[^ ]+') || INTERNALIP_DEFAULT=auto
+  SHMEXT_DEFAULT=$(echo $ENV_VARS | grep -oP 'SHMEXT=\K[^ ]+') || SHMEXT_DEFAULT=9001
+  SHMINT_DEFAULT=$(echo $ENV_VARS | grep -oP 'SHMINT=\K[^ ]+') || SHMINT_DEFAULT=10001
+  PREVIOUS_PASSWORD=$(echo $ENV_VARS | grep -oP 'DASHPASS=\K[^ ]+') || PREVIOUS_PASSWORD=none
+elif [ -f .shardeum/.env ]; then
+  echo "Existing .shardeum/.env file found. Reading settings from file."
+
+  # Read the .shardeum/.env file into a variable. Use default installer directory if it exists.
+  ENV_VARS=$(cat .shardeum/.env)
+
+  # UPDATE DEFAULT VALUES WITH SAVED VALUES
+  DASHPORT_DEFAULT=$(echo $ENV_VARS | grep -oP 'DASHPORT=\K[^ ]+') || DASHPORT_DEFAULT=8080
+  EXTERNALIP_DEFAULT=$(echo $ENV_VARS | grep -oP 'EXT_IP=\K[^ ]+') || EXTERNALIP_DEFAULT=auto
+  INTERNALIP_DEFAULT=$(echo $ENV_VARS | grep -oP 'INT_IP=\K[^ ]+') || INTERNALIP_DEFAULT=auto
+  SHMEXT_DEFAULT=$(echo $ENV_VARS | grep -oP 'SHMEXT=\K[^ ]+') || SHMEXT_DEFAULT=9001
+  SHMINT_DEFAULT=$(echo $ENV_VARS | grep -oP 'SHMINT=\K[^ ]+') || SHMINT_DEFAULT=10001
+  PREVIOUS_PASSWORD=$(echo $ENV_VARS | grep -oP 'DASHPASS=\K[^ ]+') || PREVIOUS_PASSWORD=none
 fi
 
 cat << EOF
@@ -139,16 +261,135 @@ cat << EOF
 
 EOF
 
+# read -p "Do you want to run the web based Dashboard? (Y/n): " RUNDASHBOARD
+RUNDASHBOARD=$(echo "$RUNDASHBOARD" | tr '[:upper:]' '[:lower:]')
 RUNDASHBOARD=${RUNDASHBOARD:-y}
 
+if [ "$PREVIOUS_PASSWORD" != "none" ]; then
+#  read -p "Do you want to change the password for the Dashboard? (y/N): " CHANGEPASSWORD
+#  CHANGEPASSWORD=$(echo "$CHANGEPASSWORD" | tr '[:upper:]' '[:lower:]')
+  CHANGEPASSWORD=${CHANGEPASSWORD:-n}
+else
+  CHANGEPASSWORD="y"
+fi
+
+if [ "$CHANGEPASSWORD" == "y" ]; then
+  # Hash the password using the fallback mechanism
+  DASHPASS=$(hash_password "$DASHPASS")
+else
+  DASHPASS=$PREVIOUS_PASSWORD
+  if ! [[ $DASHPASS =~ ^[0-9a-f]{64}$ ]]; then
+    DASHPASS=$(hash_password "$DASHPASS")
+  fi
+fi
+
+if [ -z "$DASHPASS" ]; then
+  echo -e "\nFailed to hash the password. Please ensure you have openssl"
+  exit 1
+fi
 
 echo # New line after inputs.
 # echo "Password saved as:" $DASHPASS #DEBUG: TEST PASSWORD WAS RECORDED AFTER ENTERED.
 
+while :; do
+#  read -p "Enter the port (1025-65536) to access the web based Dashboard (default $DASHPORT_DEFAULT): " DASHPORT
+  DASHPORT=${DASHPORT:-$DASHPORT_DEFAULT}
+  [[ $DASHPORT =~ ^[0-9]+$ ]] || { echo "Enter a valid port"; continue; }
+  if ((DASHPORT >= 125 && DASHPORT <= 65536)); then
+    DASHPORT=${DASHPORT:-$DASHPORT_DEFAULT}
+    break
+  else
+    echo "Port out of range, try again"
+  fi
+done
+
+while :; do
+#  read -p "If you wish to set an explicit external IP, enter an IPv4 address (default=$EXTERNALIP_DEFAULT): " EXTERNALIP
+  EXTERNALIP=${EXTERNALIP:-$EXTERNALIP_DEFAULT}
+
+  if [ "$EXTERNALIP" == "auto" ]; then
+    break
+  fi
+
+  # Use regex to check if the input is a valid IPv4 address
+  if [[ $EXTERNALIP =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+    # Check that each number in the IP address is between 0-255
+    valid_ip=true
+    IFS='.' read -ra ip_nums <<< "$EXTERNALIP"
+    for num in "${ip_nums[@]}"
+    do
+        if (( num < 0 || num > 255 )); then
+            valid_ip=false
+        fi
+    done
+
+    if [ $valid_ip == true ]; then
+      break
+    else
+      echo "Invalid IPv4 address. Please try again."
+    fi
+  else
+    echo "Invalid IPv4 address. Please try again."
+  fi
+done
+
+while :; do
+#  read -p "If you wish to set an explicit internal IP, enter an IPv4 address (default=$INTERNALIP_DEFAULT): " INTERNALIP
+  INTERNALIP=${INTERNALIP:-$INTERNALIP_DEFAULT}
+
+  if [ "$INTERNALIP" == "auto" ]; then
+    break
+  fi
+
+  # Use regex to check if the input is a valid IPv4 address
+  if [[ $INTERNALIP =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+    # Check that each number in the IP address is between 0-255
+    valid_ip=true
+    IFS='.' read -ra ip_nums <<< "$INTERNALIP"
+    for num in "${ip_nums[@]}"
+    do
+        if (( num < 0 || num > 255 )); then
+            valid_ip=false
+        fi
+    done
+
+    if [ $valid_ip == true ]; then
+      break
+    else
+      echo "Invalid IPv4 address. Please try again."
+    fi
+  else
+    echo "Invalid IPv4 address. Please try again."
+  fi
+done
+
+while :; do
+  echo "To run a validator on the Sphinx network, you will need to open two ports in your firewall."
+#  read -p "This allows p2p communication between nodes. Enter the first port (1025-65536) for p2p communication (default $SHMEXT_DEFAULT): " SHMEXT
+  SHMEXT=${SHMEXT:-$SHMEXT_DEFAULT}
+  [[ $SHMEXT =~ ^[0-9]+$ ]] || { echo "Enter a valid port"; continue; }
+  if ((SHMEXT >= 1025 && SHMEXT <= 65536)); then
+    SHMEXT=${SHMEXT:-9001}
+  else
+    echo "Port out of range, try again"
+  fi
+#  read -p "Enter the second port (1025-65536) for p2p communication (default $SHMINT_DEFAULT): " SHMINT
+  SHMINT=${SHMINT:-$SHMINT_DEFAULT}
+  [[ $SHMINT =~ ^[0-9]+$ ]] || { echo "Enter a valid port"; continue; }
+  if ((SHMINT >= 1025 && SHMINT <= 65536)); then
+    SHMINT=${SHMINT:-10001}
+    break
+  else
+    echo "Port out of range, try again"
+  fi
+done
+
+#read -p "What base directory should the node use (defaults to ~/.shardeum): " NODEHOME
 NODEHOME=${NODEHOME:-~/.shardeum}
 
-APPSEEDLIST="archiver-sphinx.shardeum.org"
-APPMONITOR="monitor-sphinx.shardeum.org"
+#APPSEEDLIST="archiver-sphinx.shardeum.org"
+#APPMONITOR="monitor-sphinx.shardeum.org"
+APPMONITOR="139.144.35.86"
 
 cat <<EOF
 
@@ -186,7 +427,7 @@ touch ./.env
 cat >./.env <<EOL
 EXT_IP=${EXTERNALIP}
 INT_IP=${INTERNALIP}
-EXISTING_ARCHIVERS=[{"ip":"18.194.3.6","port":4000,"publicKey":"758b1c119412298802cd28dbfa394cdfeecc4074492d60844cc192d632d84de3"},{"ip":"139.144.19.178","port":4000,"publicKey":"840e7b59a95d3c5f5044f4bc62ab9fa94bc107d391001141410983502e3cde63"},{"ip":"139.144.43.47","port":4000,"publicKey":"7af699dd711074eb96a8d1103e32b589e511613ebb0c6a789a9e8791b2b05f34"},{"ip":"72.14.178.106","port":4000,"publicKey":"2db7c949632d26b87d7e7a5a4ad41c306f63ee972655121a37c5e4f52b00a542"}]
+EXISTING_ARCHIVERS=[{"ip":"194.195.223.142","port":4000,"publicKey":"840e7b59a95d3c5f5044f4bc62ab9fa94bc107d391001141410983502e3cde63"},{"ip":"45.79.193.36","port":4000,"publicKey":"7af699dd711074eb96a8d1103e32b589e511613ebb0c6a789a9e8791b2b05f34"},{"ip":"45.79.108.24","port":4000,"publicKey":"2db7c949632d26b87d7e7a5a4ad41c306f63ee972655121a37c5e4f52b00a542"}]
 APP_MONITOR=${APPMONITOR}
 DASHPASS=${DASHPASS}
 DASHPORT=${DASHPORT}
@@ -240,6 +481,16 @@ fi
 echo "Starting image. This could take a while..."
 (docker-safe logs -f shardeum-dashboard &) | grep -q 'done'
 
+# Check if secrets.json exists and copy it inside container
+cd ${CURRENT_DIRECTORY}
+if [ -f secrets.json ]; then
+  echo "Reusing old node"
+  CONTAINER_ID=$(docker-safe ps -qf "ancestor=local-dashboard")
+  echo "New container id is : $CONTAINER_ID"
+  docker-safe cp ./secrets.json "${CONTAINER_ID}:/home/node/app/cli/build/secrets.json"
+  rm -f secrets.json
+fi
+
 #Do not indent
 if [ $RUNDASHBOARD = "y" ]
 then
@@ -264,6 +515,7 @@ To use the Command Line Interface:
 
 EOF
 
+
 echo '初始化完成'
 
 sleep 3
@@ -273,3 +525,4 @@ cd ~/.shardeum && sudo docker exec -it shardeum-dashboard operator-cli start
 cat <<EOF
 执行完成，请访问https://localhost:${DASHPORT}
 EOF
+
